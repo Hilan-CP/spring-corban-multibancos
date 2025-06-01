eec70835c6a73723839a565dc4720c9aaf4136d9
Hilan <hilan.paganini@gmail.com>
2025-06-01 19:59:53 -0300

    Testes de api de propostas

--- src/main/java/com/corbanmultibancos/business/controllers/ProposalController.java
+++ src/main/java/com/corbanmultibancos/business/controllers/ProposalController.java
@@ -7,35 +7,49 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.corbanmultibancos.business.dto.ProposalCreateDTO;
 import com.corbanmultibancos.business.dto.ProposalDataDTO;
 import com.corbanmultibancos.business.services.ProposalService;
 
 @RestController
 @RequestMapping("/proposals")
 public class ProposalController {
-	
+
 	@Autowired
 	private ProposalService proposalService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ProposalDataDTO> getProposalById(@PathVariable Long id){
+		return null;
+	}
 
 	@GetMapping
 	public ResponseEntity<Page<ProposalDataDTO>> getProposals(@RequestParam(defaultValue = "") String code,
 			@RequestParam(defaultValue = "") String employeeName,
 			@RequestParam(defaultValue = "") Integer bankCode,
-			@RequestParam(defaultValue = "generation") String dateField,
+			@RequestParam(required = true) String dateField,
 			@RequestParam(required = true) LocalDate beginDate,
 			@RequestParam(required = true) LocalDate endDate,
 			Pageable pageable){
-		System.out.println("code "+code);
-		System.out.println("employee "+employeeName);
-		System.out.println("bank "+bankCode);
-		System.out.println("field "+dateField);
-		System.out.println("begin "+beginDate);
-		System.out.println("end "+endDate);
 		Page<ProposalDataDTO> page = proposalService.getProposals(null, null, null, null, null, null, pageable);
-		return ResponseEntity.ok(page);
+		return null;
+	}
+
+	@PostMapping
+	public ResponseEntity<ProposalDataDTO> createProposal(@RequestBody ProposalCreateDTO createDto){
+		return null;
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ProposalDataDTO> updateProposal(@PathVariable Long id, @RequestBody ProposalCreateDTO createDto){
+		return null;
 	}
 }
