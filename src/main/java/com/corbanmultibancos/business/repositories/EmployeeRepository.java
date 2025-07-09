c3ac5ebdde686286e3479be107cd76f709ecbaad
Hilan <hilan.paganini@gmail.com>
2025-07-09 16:04:24 -0300

    Otimização de consultas

--- src/main/java/com/corbanmultibancos/business/repositories/EmployeeRepository.java
+++ src/main/java/com/corbanmultibancos/business/repositories/EmployeeRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.corbanmultibancos.business.entities.Employee;
@@ -14,7 +15,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	
 	boolean existsById(Long id);
 
+	@EntityGraph(attributePaths = {"team","user","user.role"})
 	Optional<Employee> findByCpf(String cpf);
 
+	@EntityGraph(attributePaths = {"team","user","user.role"})
 	Page<Employee> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+	@EntityGraph(attributePaths = {"team","user","user.role"})
+	Page<Employee> findAll(Pageable pageable);
 }
--- src/main/java/com/corbanmultibancos/business/repositories/UserRepository.java
+++ src/main/java/com/corbanmultibancos/business/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.corbanmultibancos.business.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.corbanmultibancos.business.entities.User;
@@ -9,6 +12,10 @@ import com.corbanmultibancos.business.entities.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	boolean existsById(Long id);
-	
+
+	@EntityGraph(attributePaths = "role")
 	Optional<User> findByUsername(String username);
+
+	@EntityGraph(attributePaths = "role")
+	Page<User> findAll(Pageable pageable);
 }
